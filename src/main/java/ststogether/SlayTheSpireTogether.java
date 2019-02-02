9869c6d14d4ac81d7928b7a61db8e4cd495110c7
seanD111 <sean_delong@hotmail.ca>
2019-02-02 16:20:05 -0500

    adding maven project

---
+++ src/main/java/ststogether/SlayTheSpireTogether.java
@@ -0,0 +1,13 @@
+package ststogether;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+
+@SpireInitializer
+public class SlayTheSpireTogether {
+    public SlayTheSpireTogether(){
+
+    }
+    public static void initialize(){
+
+    }
+}
